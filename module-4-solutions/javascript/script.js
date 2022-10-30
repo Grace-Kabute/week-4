0e6f70a6af5f5861d6e422499b18aaed63be1b3c
Kabute Grace <kabutemuthoni@gmail.com>
2022-10-30 20:16:10 +0300

    new change

--- module-4-solutions/javascript/script.js
+++ module-4-solutions/javascript/script.js
@@ -1,13 +1,37 @@
 (function() {
-
     let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (let i = 0; i < names.length; i++) {
-        let firstLetter = names[i].charAt(0).toLowerCase();
-        if (firstLetter === 'j') {
-            console.log(`Goodbye ${names[i]}`);
-        } else {
-            console.log(`hello ${names[i]}`);
-        }
-    }
+    (function(window) {
+        let helloSpeaker = {};
+        helloSpeaker.call = 'hello';
 
+        helloSpeaker.hello = function speak() {
+            console.log(helloSpeaker.call + ' ' + names[i]);
+        }
+        window.helloSpeaker = helloSpeaker;
+    })(window);
+
+
+    (function(window) {
+        let speakGoodbye = {};
+        speakGoodbye.call = 'goodbye';
+
+        speakGoodbye.hello = function speak() {
+            console.log(speakGoodbye.call + ' ' + names[i]);
+        }
+        window.speakGoodbye = speakGoodbye;
+    })(window);
+
+
+    for (var i = 0; i < names.length; i++) {
+
+
+        var firstLetter = names[i].charAt(0).toLowerCase();
+
+
+        if (firstLetter === 'j') {
+            speakGoodbye.hello();
+        } else {
+            helloSpeaker.hello();
+        }
+    };
 })();
