c1661fe462a9c32528e68318b40d283303a9be19
Kabute Grace <kabutemuthoni@gmail.com>
2022-10-30 09:24:11 +0300

    week 4 assignment

---
+++ module-4-solutions/javascript/script.js
@@ -0,0 +1,13 @@
+(function() {
+
+    let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (let i = 0; i < names.length; i++) {
+        let firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            console.log(`Goodbye ${names[i]}`);
+        } else {
+            console.log(`hello ${names[i]}`);
+        }
+    }
+
+})();
